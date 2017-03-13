549c5cb259fcd0673d863ca012716dce3ff7425f
Ivan Chavero <ichavero@redhat.com>
2017-03-13 16:55:40 -0700

    Add restore node script

---
+++ scripts/restore_node.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Restoring node..."
+cp  etc/moximo/scripts/moximo-setup.sh  /etc/moximo/scripts/.
+systemctl stop kube-apiserver.service   kube-controller-manager.service kube-proxy.service kube-scheduler.service kubelet.service
+systemctl disable kube-apiserver.service   kube-controller-manager.service kube-proxy.service kube-scheduler.service kubelet.service
+systemctl stop moximo-master
+systemctl disable moximo-master
+cd /etc/sysconfig/network-scripts/
+cp ifcfg-eth0:0.FIRSTBOOT ifcfg-eth0\:0
+rm /etc/moximo/.node
+rm /etc/moximo/.master
+ifup eth0:0
+
+echo "Done.."
