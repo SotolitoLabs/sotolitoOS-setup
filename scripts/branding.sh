d27b519a83cff2d49b858ab344bab216d948cc20
Ivan Chavero <ichavero@redhat.com>
2018-09-09 18:06:43 -0600

    Fix branding script

--- scripts/branding.sh
+++ scripts/branding.sh
@@ -3,7 +3,7 @@
 # Brand fedora upstream to SotolitoOS
 # Iván Chavero <ichavero@chavero.com.mx>
 
-DEFAULT_PASSWORD="sereke2018sotol!/"
+DEFAULT_PASSWORD="sereke2018sotol(/"
 DEFAULT_PATH="/run/media/${USERNAME}"
 
 if [[ $1 == "" ]]; then
@@ -19,9 +19,9 @@ ROOT="${MOUNT_PATH}/__"
 BOOT="${MOUNT_PATH}/__"
 
 echo "Branding Live system"
-hostnamectl set-hostname sotolito
-useradd -c "Sotolito OS Admin User" sotolito
+useradd -R ${ROOT} -m -c "Sotolito OS Admin User" -s /bin/bash -d /home/sotolito sotolito
 echo -e "${PASSWORD}\n${PASSWORD}" | passwd sotolito
+echo "sotolito" > ${ROOT}/etc/hostname
 cp ../etc/os-release ${ROOT}/etc/os-release
 cp ../etc/system-release ${ROOT}/etc/system-release
 mkdir ${ROOT}/etc/sotolito
