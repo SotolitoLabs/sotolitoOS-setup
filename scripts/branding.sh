c4bcc96bd5d87b0ed25dfe0d620a9ab4bbbbc86d
Ivan Chavero <ichavero@redhat.com>
2018-09-09 18:40:03 -0600

    Fix branding script

--- scripts/branding.sh
+++ scripts/branding.sh
@@ -8,19 +8,24 @@ DEFAULT_PATH="/run/media/${USERNAME}"
 
 if [[ $1 == "" ]]; then
     MOUNT_PATH=$DEFAULT_PATH
+else
+    MOUNT_PATH=$1
 fi
 
 if [[ $2 == "" ]]; then
     PASSWORD=$DEFAULT_PASSWORD
+else
+    PASSWORD=$2
 fi
 
 
 ROOT="${MOUNT_PATH}/__"
-BOOT="${MOUNT_PATH}/__"
+BOOT="${MOUNT_PATH}/__boot"
 
-echo "Branding Live system"
-useradd -R ${ROOT} -m -c "Sotolito OS Admin User" -s /bin/bash -d /home/sotolito sotolito
-echo -e "${PASSWORD}\n${PASSWORD}" | passwd sotolito
+echo "Branding system on ${MOUNT_PATH}"
+echo "sotolito:x:1003:" >> ${ROOT}/etc/group
+echo "sotolito:x:1002:1003:Sotolito OS Admin User:/home/sotolito:/bin/bash" >> ${ROOT}/etc/passwd
+echo 'sotolito:$6$vtY7U03GVzGOkyvU$SwFIJc4nf7WLEVRSJV.DOhqBAO0dgUILZ/aONrnjph9x2WqJ7UBz0hpu8Xjr.vwPjuE9gQDin5rG2ST1axdtR/:17784:0:99999:7:::' >> ${ROOT}/etc/shadow
 echo "sotolito" > ${ROOT}/etc/hostname
 cp ../etc/os-release ${ROOT}/etc/os-release
 cp ../etc/system-release ${ROOT}/etc/system-release
