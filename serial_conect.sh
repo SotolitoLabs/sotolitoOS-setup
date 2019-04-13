1c290273d92f625e623a796af2ae8fd0025dd1e2
Ivan Chavero <ichavero@chavero.com.mx>
2019-04-13 14:35:28 -0500

    Add check if running inside a screen session

--- serial_conect.sh
+++ serial_conect.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#Don't run if we're inside a screen session as this can cause keystroke problems
+if [[ ${STY} != "" ]]; then
+    echo "Can't run inside a screen session"
+    exit
+fi
 echo "Connecting to Cubietruck plus serial"
-screen -S serial -t 'Sotolito Serial' /dev/ttyUSB0 115200
+
+sudo screen -S serial -t 'Sotolito Serial' /dev/ttyUSB0 115200
