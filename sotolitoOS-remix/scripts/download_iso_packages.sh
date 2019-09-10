b02f155f0f1dcf3e9b9fa5d89c9db6cea1e5382a
Ivan Chavero <ichavero@chavero.com.mx>
2019-09-09 21:41:31 -0500

    Add package dependencies

--- sotolitoOS-remix/scripts/download_iso_packages.sh
+++ sotolitoOS-remix/scripts/download_iso_packages.sh
@@ -2,7 +2,8 @@
 
 echo "Downloading packages"
 yum update -y --downloadonly --downloaddir=/var/preserve 
-yum install -y --downloadonly --downloaddir=/var/preserve cockpit git ansible skopeo podman Networkanager
+yum install -y --downloadonly --downloaddir=/var/preserve cockpit git ansible skopeo podman Networkanager NetworkManager-team NetworkManager-tui NetworkManager-wifi device-mapper \
+  device-mapper-event device-mapper-event-libs lvm2
 #rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 #rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 #The container needs some packages in order to download the kernel
