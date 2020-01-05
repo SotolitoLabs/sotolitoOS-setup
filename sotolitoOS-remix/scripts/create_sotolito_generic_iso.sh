e933594edd8c7611045d53ae2bf08bf419916b98
Ivan Chavero <ichavero@chavero.com.mx>
2020-01-04 20:30:48 -0600

    Fix iso path

--- sotolitoOS-remix/scripts/create_sotolito_generic_iso.sh
+++ sotolitoOS-remix/scripts/create_sotolito_generic_iso.sh
@@ -3,5 +3,5 @@
 rm sotolito-iso/SotolitoOS-Stream-x86_64-8-Stream-generic.iso
 rm log-sotolito.log
 ./create-iso.sh generic 2>&1 | tee log-sotolito.log
-cp sotolito-iso/SotolitoOS-7-x86_64-Minimal-1810-* ~/space/virt-images
+cp sotolito-iso/SotolitoOS-Stream-x86_64-8-Stream-generic.iso ~/space/virt-images/
 
