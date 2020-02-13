6d64ed39d01c4abf117b6704a7cc8a903a9326d0
Ivan Chavero <ichavero@chavero.com.mx>
2020-02-13 13:30:57 -0600

    Update generic iso script for variable image path

--- sotolitoOS-remix/scripts/create_sotolito_generic_iso.sh
+++ sotolitoOS-remix/scripts/create_sotolito_generic_iso.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+VIRT_IMAGES_PATH="~/space/virt-images"
+
 rm sotolito-iso/SotolitoOS-Stream-x86_64-8-Stream-generic.iso
 rm log-sotolito.log
 ./create-iso.sh generic 2>&1 | tee log-sotolito.log
-cp sotolito-iso/SotolitoOS-Stream-x86_64-8-Stream-generic.iso ~/space/virt-images/
+sudo cp sotolito-iso/SotolitoOS-Stream-x86_64-8-Stream-generic.iso ${VIRT_IMAGES_PATH}/.
 
