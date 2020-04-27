ca4f06c6b28314cf6837747dc2239353d8fe0e02
Ivan Chavero <ichavero@chavero.com.mx>
2020-04-26 21:11:25 -0500

    Update to version 8.1
    - Update to elrepo kernel-ml 5.6.2
    - Add repo2module installation for generating modular repositories
    - create modular ISO repo
    - Fix versioning
    - Update squashfs
    - Remove package donwloader container
    - Add ansible hardening playbooks
    - Fix branding
    - Change codename to Octagesimal in honor of Octavio

--- sotolitoOS-remix/scripts/create-iso.sh
+++ sotolitoOS-remix/scripts/create-iso.sh
@@ -10,23 +10,27 @@
 PREPARER="imcsk8"
 BASE_IMAGE="CentOS-Stream-x86_64-boot.iso"
 KICKSTART_FILE="sotolitoOS-generic.ks"
-KERNEL_ML_PACKAGE="kernel-ml-5.4.2-1.el7.elrepo.x86_64.rpm "
-KERNEL_ML_TOOLS_PACKAGE="kernel-ml-tools-5.4.2-1.el7.elrepo.x86_64.rpm"
-KERNEL_MIRROR="http://repos.lax-noc.com/elrepo/archive/kernel/el7/x86_64/RPMS/"
+KERNEL_ML_PACKAGE="kernel-ml-5.6.2-1.el8.elrepo.x86_64.rpm"
+KERNEL_ML_TOOLS_PACKAGE="kernel-ml-tools-5.6.2-1.el8.elrepo.x86_64.rpm"
+KERNEL_MIRROR="http://repos.lax-noc.com/elrepo/archive/kernel/el8/x86_64/RPMS/"
 #ISO_NAME="SotolitoOS-Stream-x86_64-master.iso"
 ISO_NAME="SotolitoOS-Stream-x86_64-generic.iso"
 EL_REPO="https://www.elrepo.org"
-EL_REPO_RPM="elrepo-release-7.0-4.el7.elrepo.noarch.rpm"
+EL_REPO_RPM="elrepo-release-8.el8.elrepo.noarch.rpm"
 EL_REPO_RPM_GPG="RPM-GPG-KEY-elrepo.org"
+EPEL_REPO="https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"
 FULL_PATH=$(realpath .)
-PACKAGES_PATH="${FULL_PATH}/sotolito-iso/iso-dev/isolinux/Packages/"
+#PACKAGES_PATH="${FULL_PATH}/sotolito-iso/iso-dev/isolinux/Packages/"
+PACKAGES_PATH="${FULL_PATH}/sotolito-iso/iso-dev/Packages/"
+
+echo "DEBUG: PACKAGES PATH: ${PACKAGES_PATH}"
 
 #BASE_IMAGE_URL="http://mirrors.usc.edu/pub/linux/distributions/centos/7.6.1810/isos/x86_64/${BASE_IMAGE}"
 BASE_IMAGE_URL="http://mirror.keystealth.org/centos/8-stream/isos/x86_64/CentOS-Stream-8-x86_64-20191219-boot.iso"
 KERNEL_ML_PACKAGE_URL="${KERNEL_MIRROR}/${KERNEL_ML_PACKAGE}"
 KERNEL_ML_TOOLS_PACKAGE_URL="${KERNEL_MIRROR}/${KERNEL_ML_TOOLS_PACKAGE}"
 # We use the same version as the centos base image
-VERSION="8-Stream"
+VERSION="8.1-Stream"
 
 DATE=`date +%Y-%m-%d`
 APPID="SotolitoLabs - ${DATE} - ${VERSION}"
@@ -35,18 +39,31 @@ DEFAULT_SSH_KEYS_PATH="~/space/sotolitoLabs/security/ssh/"
 
 #SQUASHFS_FILE="squashfs-sotolito.img"
 SQUASHFS_FILE="squashfs-sotolito-stream.img"
+SQUASHFS_URL="https://sotolitolabs.com/dist/centos/8/x86_64/branding/img/${SQUASHFS_FILE}"
 
 if [[ "${1}" != "" ]]; then
-	KICKSTART_FILE="sotolitoOS-${1}.ks"
+    KICKSTART_FILE="sotolitoOS-${1}.ks"
     ISO_NAME="SotolitoOS-Stream-x86_64-${VERSION}-${1}.iso"
 fi
 
 echo "Installing required packages"
-sudo dnf install -y createrepo genisoimage podman
+sudo dnf install -y createrepo_c createrepo genisoimage podman git
+
+echo "Checkig for repo2module"
+if [[ "$(command -v repo2moduleee)" != "" ]]; then
+    echo "repo2module detected, continuing"
+else
+    echo "missing repo2module, installing..."
+    git clone https://github.com/sgallagher/repo2module.git
+    cd repo2module
+    python3 setup.py install --user
+    cd ..
+fi
 
 echo "Creating the Enterprise SotolitoOS ISO instller"
 mkdir -p sotolito-iso/iso-dev/{isolinux,images,ks,EFI,postinstall}
-mkdir -p sotolito-iso/iso-dev/isolinux/Packages
+#mkdir -p sotolito-iso/iso-dev/isolinux/Packages
+mkdir -p sotolito-iso/iso-dev/Packages
 cd sotolito-iso
 echo "Downloading base image"
 wget -c $BASE_IMAGE_URL -O $BASE_IMAGE
@@ -60,22 +77,27 @@ rsync -av iso/images/ iso-dev/images/
 sudo rsync -av iso/EFI/ iso-dev/EFI/
 
 echo "Downloading extra packages"
-cd iso-dev/isolinux/Packages
-cp ../../../../files/RPMS/sotolitoos-release*8* .
+#cd iso-dev/isolinux/Packages
+cd iso-dev/Packages
+cp ../../../../files/RPMS/sotolitoos-release-8* .
 wget -c "${EL_REPO}/${EL_REPO_RPM}"
 wget -c "${EL_REPO}/${EL_REPO_RPM_GPG}"
-wget -c $KERNEL_ML_PACKAGE_URL
-wget -c $KERNEL_ML_TOOLS_PACKAGE_URL
+#wget -c $KERNEL_ML_PACKAGE_URL
+#wget -c $KERNEL_ML_TOOLS_PACKAGE_URL
+wget -c $EPEL_REPO
 
 cp "${FULL_PATH}/download_iso_packages.sh" $PACKAGES_PATH
-podman run --rm -ti --name=tmp-centos-pkgs -v $PACKAGES_PATH:/var/preserve registry.centos.org/centos/centos:8 /var/preserve/download_iso_packages.sh
+/usr/bin/podman run --rm -ti --name=tmp-centos-pkgs -v $PACKAGES_PATH:/var/preserve registry.centos.org/centos/centos:8 /var/preserve/download_iso_packages.sh
 
 echo "Create image repo"
 cd ..
-createrepo .
+rm -rf repodata
+createrepo_c -g ../../files/sotolito-comps-BaseOS.x86_64.xml  .
+repo2module --module-name=sotolito-core --module-context sotolito1 . modules.yaml
+modifyrepo_c --mdtype=modules modules.yaml repodata
+rm modules.yaml
 
 echo "Adding Sotolito kickstart file to ISO"
-cd ..
 cp ../../../ks/$KICKSTART_FILE ks/sotolitoOS.ks
 
 echo "Copy branding for cockpit"
@@ -84,8 +106,17 @@ mkdir postinstall/branding
 cp -rp ../../../../usr/share/cockpit/branding/sotolito postinstall/branding/
 cp ../../files/dhcpd.conf postinstall/
 cp ../../../../etc/profile.d/sotolito_env.sh postinstall/
+# We like ansible
+mkdir postinstall/ansible
 # We need our playbooks
-cp -rp ../../../../ansible postinstall/
+cp -rp ../../../../ansible postinstall/ansible/sotolito
+# Install ansible-hardening role for STIG compliance
+if [[ ! -d postinstall/ansible/roles/ansible-hardening ]]; then
+    echo "Downloading ansible-hardening role"
+    mkdir -p postinstall/ansible/roles
+    git clone https://github.com/openstack/ansible-hardening postinstall/ansible/roles/ansible-hardening
+fi
+
 # SELinux shit
 cp -rp ../../../../selinux postinstall/
 cp ../../../../../security/ssh/* postinstall/
@@ -94,11 +125,11 @@ cd isolinux
 echo "Branding Image"
 echo "Branding Boot menu"
 sed -i 's/CentOS/SotolitoOS/' isolinux.cfg
-sed -i 's/CentOS/SotolitoOS/' grub.cfg
+#sed -i 's/CentOS/SotolitoOS/' grub.cfg
 sed -i 's/CentOS/SotolitoOS/' grub.conf
-sed -i 's/x86_64-dvd quiet/x86_64-dvd quiet ks=cdrom:\/ks\/sotolitoOS.ks/' isolinux.cfg
-sed -i '/menu default/d' isolinux.cfg
-sed -i 's/sotolitoOS.ks/sotolitoOS.ks\n  menu default/' isolinux.cfg
+#sed -i 's/-x86_64-dvd/ rd.retry=10 rd.timeout=15 \n menu default/' isolinux.cfg
+sed -i 's/menu default//' isolinux.cfg
+sed -i 's/-x86_64-dvd quiet/ quiet rd.retry=10 rd.timeout=15  ks=cdrom:\/ks\/sotolitoOS.ks\n  menu default/' isolinux.cfg
 
 # poor man's branding
 echo "Branding Initrd"
@@ -110,9 +141,10 @@ sudo cp ../../../files/images/branding/sotolitoLabs_original_white_distro.png us
 sudo sed -i 's/CentOS/SotolitoOS/' .buildstamp
 sudo sed -i 's/CentOS/SotolitoOS/' etc/initrd-release
 sudo sed -i 's/dracut/SotolitoOS/' etc/initrd-release
-sudo sed -i 's/Core/Horilka/' etc/initrd-release
+#Changed the name in honor of Octavio Mendez (octagesimal) sudo sed -i 's/Core/Horilka/' etc/initrd-release
+sudo sed -i 's/Core/Octagesimal/' etc/initrd-release
 sudo sed -i 's/rmdir/rm -rf/' usr/sbin/fetch-kickstart-disk
-sudo find . | cpio --create --format='newc' > ../isolinux/initrd.img
+sudo find . | sudo cpio --create --format='newc' > ../isolinux/initrd.img
 cd ../isolinux
 sudo xz --format=lzma initrd.img
 sudo mv initrd.img.lzma initrd.img
@@ -121,9 +153,10 @@ sudo rm -rf ../tmp_initrd
 cd ..
 chmod +w images
 echo "Adding sotolito squashfs"
-cp "../../../files/${SQUASHFS_FILE}" images/install.img
+wget -c ${SQUASHFS_URL} -O images/install.img
 echo "Add product.img for branding the installer"
 cp ../../../anaconda/branding/product.img images/
+
 
 echo "Generate ISO image"
 sudo mkisofs -o "../${ISO_NAME}" \
@@ -132,12 +165,15 @@ sudo mkisofs -o "../${ISO_NAME}" \
     -b isolinux/isolinux.bin \
     -c isolinux/boot.cat \
     -no-emul-boot \
-    -V 'SotolitoOS 8-Stream x86_64' \
+    -V 'SotolitoOS-Stream-8' \
     -boot-load-size 4 \
     -boot-info-table \
     -l -r -R -v -T \
     .
 
+
 sudo isohybrid "../${ISO_NAME}"
 sudo chown $USER "../${ISO_NAME}"
-echo "Be happy, drink a beer and a shot of sotol"
+cd ..
+echo "Your iso is ready at: ${PWD}/${ISO_NAME} sir"
+echo "share it with your friends and be happy, drink a beer and a shot of sotol"
