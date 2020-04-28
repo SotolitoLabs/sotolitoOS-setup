26db9733937e5241ec018a5686ffa5f38c5922ea
Ivan Chavero <ichavero@chavero.com.mx>
2020-04-28 03:01:54 -0500

    Update ISO version

--- sotolitoOS-remix/scripts/create_sotolito_generic_iso.sh
+++ sotolitoOS-remix/scripts/create_sotolito_generic_iso.sh
@@ -2,8 +2,8 @@
 
 VIRT_IMAGES_PATH="/var/lib/libvirt/images"
 
-rm SotolitoOS-Stream-x86_64-8.1-Stream-generic.iso
+rm SotolitoOS-Stream-x86_64-8.3-Stream-generic.iso
 ./create-iso.sh generic 2>&1 | tee log-sotolito.log
 echo "Copying ISO to $VIRT_IMAGES_PATH"
-sudo cp sotolito-iso/SotolitoOS-Stream-x86_64-8.1-Stream-generic.iso "${VIRT_IMAGES_PATH}/SotolitoOS-Stream-x86_64-8.1-Stream-generic.iso"
+sudo cp sotolito-iso/SotolitoOS-Stream-x86_64-8.3-Stream-generic.iso "${VIRT_IMAGES_PATH}/SotolitoOS-Stream-x86_64-8.3-Stream-generic.iso"
 
